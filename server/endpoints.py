b3973abd15455c619d0da396bd326477af5bb8a4
sgdorry <sd@stevendorry.com>
2025-10-07 22:51:28 +0200

    Fixed whitespaces in endpoints

--- server/endpoints.py
+++ server/endpoints.py
@@ -47,7 +47,7 @@ class Endpoints(Resource):
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
-        
+
 
 @api.route('/health')
 class Health(Resource):
