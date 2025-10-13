61afb912e3d0c8abed0fbb04fd6e98d306fa1ce5
3btesham <ebtesham90@gmail.com>
2025-10-13 18:20:10 -0400

    commiting test_good_create which checks if a good state was made

--- states/tests/test_queries.py
+++ states/tests/test_queries.py
@@ -1,12 +1,17 @@
 from unittest.mock import patch
 import pytest
 
-import states.queries as qry
+import queries as qry
 
 def test_num_states():
-    #current count of states in database
-    old_count = qry.num_states()
-    #adding a new state
-    qry.create(qry.SAMPLE_STATE)
-    #checking if a new state was created
-    assert qry.num_states == old_count + 1
+    old_count = qry.num_states() #current count of states in database
+    qry.create(qry.SAMPLE_STATE) #adding a new state
+    assert qry.num_states == old_count + 1 #checking if a new state was created
+
+    
+def test_good_create():
+    old_count = qry.num_states() #current count of states
+    new_rec_id = qry.create(qry.SAMPLE_STATE) #new record 
+    assert qry.is_valid_id(new_rec_id) #checking if the new id created is a valid one
+    assert qry.num_states == old_count + 1 #sees if the new state was created
+    
