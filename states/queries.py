7082888e67440a4b3dfb61afeb6454c5115120d8
Mattias Larsson <ml8910@nyu.edu>
2025-10-14 18:12:22 +0200

    Added db_connect and read functions

--- states/queries.py
+++ states/queries.py
@@ -26,6 +26,28 @@ def create(fields: dict):
     if (not isinstance(fields, dict)):
          raise ValueError(f'Bad type for {type(fields)=}')
     if (not fields.get(NAME)):
-        raise ValueError(f'Bad value for {fields.get(NAME)=}')new_id = str(len(state_cache) + 1)
+        raise ValueError(f'Bad value for {fields.get(NAME)=}')
+        new_id = str(len(state_cache) + 1)
         state_cache[new_id] = fields
         return new_id
+
+'''Connects to MongoDB database'''
+def db_connect():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["statesdb"] 
+    return db
+
+"""
+Reads documents from the MongoDB
+"""
+def read(state_id=None):
+    db = db_connect()
+    collection = db["states"]
+
+    if state_id is None:
+        # return all states as a list
+        return list(collection.find({}, {"_id": 0}))
+    else:
+        # find one state by its 'id'
+        result = collection.find_one({"id": state_id}, {"_id": 0})
+        return result
