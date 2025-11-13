e5f0a1070d540a4475bd43321b4ab4279ddd9713
JtheSloth <jsd9975@nyu.edu>
2025-11-13 16:53:03 -0500

    Added bad president test to countries test file

--- countries/tests/test_queries_countries.py
+++ countries/tests/test_queries_countries.py
@@ -55,3 +55,11 @@ def test_create_bad_capital():
         # try creating a country with an int as the value for capital
         qry.create({'name': 'United States of America', 'population': 100 , 'continent': 'North America', 'capital': 45, 'gdp': '1.2 T', 'area': '1000 sq mi', 'founded': '1900', 'president': 'Test Person'})
     assert qry.num_countries() == old_count 
+
+
+def test_create_bad_president():
+    old_count = qry.num_countries()  # current count of countries
+    with pytest.raises(Exception):
+        # try creating a country with an int as the value for president
+        qry.create({'name': 'United States of America', 'population': 100 , 'continent': 'North America', 'capital': 'DC', 'gdp': '1.2 T', 'area': '1000 sq mi', 'founded': '1900', 'president': 55})
+    assert qry.num_countries() == old_count
