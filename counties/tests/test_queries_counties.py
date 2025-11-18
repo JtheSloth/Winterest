6c302b4f71d762716fa255d50c9192401898bff5
3btesham <ebtesham90@gmail.com>
2025-11-18 18:57:32 -0500

    11/18/25 Commit 1: Adding test_read to counties

--- counties/tests/test_queries_counties.py
+++ counties/tests/test_queries_counties.py
@@ -34,6 +34,26 @@ def test_create_bad_param_type():
     with pytest.raises(Exception):
         qry.create([1, 2, 3])
     assert qry.num_counties() == old_count #make sure number of counties did not change
+    
+@patch('queries_counties.db_connect')
+def test_read(mock_db_connect, temp_county):
+    # create a test county
+    # new_rec_id = qry.create(qry.SAMPLE_COUNTY)
+
+    # mock the MongoDB collection
+    mock_collection = mock_db_connect.return_value.__getitem__.return_value
+    mock_collection.find_one.return_value = {'id': temp_county, 'name': 'Queens'}
+    mock_collection.find.return_value = [{'id': '1', 'name': 'Queens'}, {'id': '2', 'name': 'Bronx'}]
+
+    # test reading that specific county
+    result = qry.read(temp_county)
+    assert result is not None
+    assert result['id'] == temp_county
+
+    # test reading all counties
+    all_counties = qry.read()
+    assert isinstance(all_counties, list)
+    assert len(all_counties) > 0
 
 
 @patch('queries_counties.db_connect', return_value=False, autospec=True)
