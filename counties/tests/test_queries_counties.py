3c96e9f245dddff8353862e4af2e8e0c295f9501
3btesham <ebtesham90@gmail.com>
2025-11-18 19:26:02 -0500

    small edit in naming convention

--- counties/tests/test_queries_counties.py
+++ counties/tests/test_queries_counties.py
@@ -46,7 +46,7 @@ def test_create_bad_param_type():
         qry.create([1, 2, 3])
     assert qry.num_counties() == old_count #make sure number of counties did not change
     
-def test_create_bad_state(county):
+def test_create_bad_state(county_delta):
     with county_delta():
         with pytest.raises(Exception):
             qry.create({'id': '1', 'name': 'Bronx', 'population': 1472654, 'state': 2305354, 'area': '42.2 sq miles', 'founded': '1914','county_seat': 'Bronx Borough Hall'})
